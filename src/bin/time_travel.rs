b2cbc45b8808d967a055ec790e8d10fc8d3311bf
LiXiang <rickanmorty@163.com>
2024-03-22 20:44:17 +0800

    DeBug

--- src/bin/time_travel.rs
+++ src/bin/time_travel.rs
@@ -1,6 +1,13 @@
 fn main() {
-    for number in (1..4).rev() {
-        println!("{}!", number);
+    println!("Hello world!");
+    let res: i32 = select_number(42);
+    println!("{}", res);
+}
+
+fn select_number(x: i32) -> i32 {
+    if x > 0 {
+        return x;
+    } else {
+        return -x;
     }
-    println!("LIFTOFF!!!");
 }
